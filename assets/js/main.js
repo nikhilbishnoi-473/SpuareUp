329fcbbfb0d27ba215efccb001d16478dd7d002e
Nikhil Bishnoi <nikhilbishnoi@Nikhils-MacBook-Air.local>
2024-06-03 15:28:39 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+  }
+
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
+  });
+});
+
+
+$('.responsive_1').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  variableWidth: true,
+  slidesToShow: 6,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 2000,
+  pauseOnHover: true,
+  cssEase: 'linear',
+});
+ 
